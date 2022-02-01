d2496aa910267537da099e9298c07774c2537ead
Rohan Chitnis <ronuchit@mit.edu>
2022-02-01 17:16:47 -0500

    add created/expanded nodes

--- analysis/run_supercloud_experiments.sh
+++ analysis/run_supercloud_experiments.sh
@@ -12,33 +12,38 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     python $FILE --experiment_id cover_branchfac --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function branching_factor
     python $FILE --experiment_id cover_energy --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_energy_lookaheaddepth0
     python $FILE --experiment_id cover_count --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_count_lookaheaddepth0
-    python $FILE --experiment_id cover_expnodes --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes
+    python $FILE --experiment_id cover_expnodescreated --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_created
+    python $FILE --experiment_id cover_expnodesexpanded --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_expanded
 
     # blocks
     python $FILE --experiment_id blocks_prederror --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function prediction_error
     python $FILE --experiment_id blocks_branchfac --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function branching_factor
     python $FILE --experiment_id blocks_energy --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_energy_lookaheaddepth0
     python $FILE --experiment_id blocks_count --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_count_lookaheaddepth0
-    python $FILE --experiment_id blocks_expnodes --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes
+    python $FILE --experiment_id blocks_expnodescreated --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_created
+    python $FILE --experiment_id blocks_expnodesexpanded --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_expanded
 
     # painting
     python $FILE --experiment_id painting_prederror --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function prediction_error
     python $FILE --experiment_id painting_branchfac --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function branching_factor
     python $FILE --experiment_id painting_energy --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_energy_lookaheaddepth0
     python $FILE --experiment_id painting_count --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_count_lookaheaddepth0
-    python $FILE --experiment_id painting_expnodes --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes
+    python $FILE --experiment_id painting_expnodescreated --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_created
+    python $FILE --experiment_id painting_expnodesexpanded --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_expanded
 
     # painting with lid always open
     python $FILE --experiment_id painting_always_open_prederror --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function prediction_error
     python $FILE --experiment_id painting_always_open_branchfac --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function branching_factor
     python $FILE --experiment_id painting_always_open_energy --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_energy_lookaheaddepth0
     python $FILE --experiment_id painting_always_open_count --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_count_lookaheaddepth0
-    python $FILE --experiment_id painting_always_open_expnodes --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes
+    python $FILE --experiment_id painting_always_open_expnodescreated --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_created
+    python $FILE --experiment_id painting_always_open_expnodesexpanded --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_expanded
 
     # cluttered_table
     python $FILE --experiment_id ctable_prederror --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function prediction_error
     python $FILE --experiment_id ctable_branchfac --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function branching_factor
     python $FILE --experiment_id ctable_energy --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_energy_lookaheaddepth0
     python $FILE --experiment_id ctable_count --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function lmcut_count_lookaheaddepth0
-    python $FILE --experiment_id ctable_expnodes --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes
+    python $FILE --experiment_id ctable_expnodescreated --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_created
+    python $FILE --experiment_id ctable_expnodesexpanded --env cluttered_table --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_score_function expected_nodes_expanded
 done
